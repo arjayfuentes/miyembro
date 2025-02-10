47fcc626a48d4bb12ef4f7481a09c0e697ef7c8c
Arjay Fuentes <arjay@gmail.com>
2025-02-10 19:17:07 +0100

    working google signin

--- src/app/core/auth/pages/login/login.component.ts
+++ src/app/core/auth/pages/login/login.component.ts
@@ -1,62 +1,40 @@
-import { Component, ElementRef, OnInit, ViewChild, inject } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../../services/authentication.service';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { SessionService } from '../../services/session.service';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ErrorResponse } from 'src/app/core/models/error-response';
+import { IftaLabelModule } from 'primeng/iftalabel';
+import { InputTextModule } from 'primeng/inputtext';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { PasswordModule } from 'primeng/password';
+import { CommonModule } from '@angular/common';
 import { Subject, takeUntil } from 'rxjs';
 import { AlertService } from 'src/app/shared/services/alert.service';
 import { GoogleLoginRequest } from 'src/app/core/models/google-login-request';
-import { ButtonModule } from 'primeng/button';
-import { DatePickerModule } from 'primeng/datepicker';
-import { IftaLabelModule } from 'primeng/iftalabel';
-import { InputTextModule } from 'primeng/inputtext';
-import { PasswordModule } from 'primeng/password';
 
-import {
-  SocialLoginModule,
-  SocialAuthServiceConfig,
-  SocialAuthService,
-  GoogleLoginProvider,
-} from "@abacritt/angularx-social-login";
+declare const google: any;
 
 @Component({
   selector: 'app-login',
+  imports: [FormsModule, ReactiveFormsModule, CommonModule, PasswordModule, ButtonModule, CardModule, IftaLabelModule, InputTextModule],
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss'],
-  imports: [FormsModule, ReactiveFormsModule, DatePickerModule, IftaLabelModule, InputTextModule, ButtonModule, PasswordModule],
-  providers: [
-    SocialAuthService,
-    {
-      provide: 'SocialAuthServiceConfig',
-      useValue: {
-        autoLogin: false,
-        providers: [
-          {
-            id: GoogleLoginProvider.PROVIDER_ID,
-            provider: new GoogleLoginProvider('654581949282-dmvkqbivaa8rmvem7ipjbas30p5akkrm.apps.googleusercontent.com', {
-              scopes: 'openid profile email',
-            }),
-          },
-        ],
-        onError: (err) => {
-          console.error(err);
-        },
-      } as SocialAuthServiceConfig,
-    }
-  ],
+  styleUrl: './login.component.scss'
 })
 export class LoginComponent implements OnInit {
 
   loginForm: FormGroup;
   loginErrorMessage: string | null = null;
+  private redirectURL: string | null = null;
   private unsubscribe: Subject<any> = new Subject();
-
+  
   constructor(
+    private activatedRoute: ActivatedRoute,
     private authenticationService: AuthenticationService,
     private router: Router,
     private sessionService: SessionService,
-    private alertService: AlertService,
-    private socialAuthService: SocialAuthService
+    private alertService: AlertService
   ) {
     this.loginForm = new FormGroup({
       email: new FormControl('', Validators.required),
@@ -65,11 +43,51 @@ export class LoginComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.loginForm.valueChanges.pipe(takeUntil(this.unsubscribe)).subscribe((val) => {
-      if (val && this.loginErrorMessage) {
-        this.loginErrorMessage = null;
-      }
+    const params = this.activatedRoute.snapshot.queryParams;
+    const fieldRedirect = 'redirectTo';
+    this.loginForm
+      .valueChanges.pipe(takeUntil(this.unsubscribe))
+      .subscribe((val) => {
+        if (val && this.loginErrorMessage) {
+          this.loginErrorMessage = null;
+        }
+      });
+      this.initializeGoogleSignIn();
+  } 
+
+  initializeGoogleSignIn() {
+    google.accounts.id.initialize({
+      client_id: '654581949282-dmvkqbivaa8rmvem7ipjbas30p5akkrm.apps.googleusercontent.com',
+      callback: this.handleCredentialResponse.bind(this),
+      scope: 'profile email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.address', // Add the address scope
+      prompt: 'select_account' // To prompt for account selection every time
     });
+    google.accounts.id.renderButton(
+      document.getElementById('googleLoginButton'),
+      { theme: 'outline', size: 'large', text: 'continue_with' }
+    );
+    
+    google.accounts.id.prompt(); 
+  }
+
+  handleCredentialResponse(response: any) {
+    const googleToken = response.credential;
+
+    
+    const googleLoginRequest: GoogleLoginRequest = {
+      googleToken: googleToken
+    };
+
+    this.authenticationService.loginWithGoogle(googleLoginRequest).subscribe(
+      (res) => {
+        this.sessionService.setSession(res);  
+        this.successFulLogin();
+      },
+      (err: any) => {
+        this.loginErrorMessage = err.error.message;
+        this.alertService.error('/login', 'Error', err.error.message);
+      }
+    );
   }
 
   ngOnDestroy(): void {
@@ -77,7 +95,8 @@ export class LoginComponent implements OnInit {
     this.unsubscribe.complete();
   }
 
-  onClickTraditionalLogin() {
+
+  onClickLogin() {
     const loginFormVal = this.loginForm.value;
     this.loginErrorMessage = null;
     this.authenticationService.login(loginFormVal).subscribe(
@@ -92,30 +111,16 @@ export class LoginComponent implements OnInit {
     );
   }
 
-  onClickGoogleLogin(response: any) {
-
-    console.log(response);
-
-    const googleToken = response.credential;  // Token received from Google
-    
-    const googleLoginRequest: GoogleLoginRequest = {
-      googleToken: googleToken
-    };
-
-    console.log(googleLoginRequest);
-  
-    this.authenticationService.loginWithGoogle(googleLoginRequest).subscribe(
-      (res) => {
-        this.sessionService.setSession(res);  
-        this.successFulLogin();
-      },
-      (err: any) => {
-        this.loginErrorMessage = err.error.message;
-        this.alertService.error('/login', 'Error', err.error.message);
-      }
-    );
+  onClickRegister() {
+    this.router.navigate(['/register']);
   }
-  
+
+  handleKeyDown(event: KeyboardEvent) {
+    if (event.key === 'Enter' || event.key === ' ') {
+      this.onClickRegister();
+    }
+  }
+
   successFulLogin() {
     console.log(this.sessionService.getSession());
     const numberOfJoinedOrganizations: number | undefined = this.sessionService.getSession()?.organizationIdsOfMember?.length;
@@ -131,7 +136,4 @@ export class LoginComponent implements OnInit {
     }
   }
 
-  onClickRegister() {
-    this.router.navigate(['/register']);
-  }
 }
